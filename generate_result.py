8bcec1cfd42b9119f3ea358d63a577f6313efd9e
Jef-H <jeffery.s.hinton@gmail.com>
2024-05-11 10:40:28 -0500

    add log file and format

--- generate_result.py
+++ generate_result.py
@@ -1,16 +1,19 @@
 from itertools import product
 
+
 def process_combination(param1, param2, param3):
     # Example operation: Concatenate the parameters and return the result
-    result = f'{param1}-{param2}-{param3}'  # Example operation
+    result = f"{param1}-{param2}-{param3}"  # Example operation
     return result
 
-def example_function(a,  b, c):
+
+def example_function(a, b, c):
     return "167"
+
 
 def generate_combination_and_results(params_levels, process_fuction):
     # Example usage:
-    #params_levels = [['A', 'B', 'C'], [1, 2], ['X', 'Y']]
+    # params_levels = [['A', 'B', 'C'], [1, 2], ['X', 'Y']]
     combinations = []
     results = []
 
@@ -22,19 +25,44 @@ def generate_combination_and_results(params_levels, process_fuction):
         results.append(result)
     return combinations, results
 
+
 # Print or use the combinations and results
 def print_combo_and_result(combinations, results):
     for combination, result in zip(combinations, results):
-        print(f'{combination} {result}')
+        print(f"{combination} {result}")
+
 
 def pretty_print_combo_and_result_together(combinations, results):
-        for combination, result in zip(combinations, results):
-            print(f'[Combo: {combination}, {result}]')
+    for combination, result in zip(combinations, results):
+        print(f"[Combo: {combination}, {result}]")
 
-#example usage: 
+
+def write_log_file(example_function, combinations, results):
+    log_file_name = example_function.__name__ + "_log.txt"
+    with open(log_file_name, "w") as log_file:
+        log_file.write("Combinations=")
+        log_file.write("[")
+        for combo in combinations:
+            log_file.write(str(combo))
+            log_file.write(",")
+        log_file.write("]")
+        log_file.write("\n")
+        log_file.write("Results=")
+        log_file.write("[")
+        for res in results:
+            log_file.write(res)
+            log_file.write(",")
+        log_file.write("]")
+
+
+# example usage:
 def demo_the_goodies():
-    params_levels = [['A', 'B', 'C'], [1, 2], ['X', 'Y']]
-    combinations, results = generate_combination_and_results(params_levels, example_function)
+    params_levels = [["A", "B", "C"], [1, 2], ["X", "Y"]]
+    combinations, results = generate_combination_and_results(
+        params_levels, example_function
+    )
     pretty_print_combo_and_result_together(combinations, results)
+    write_log_file(example_function, combinations, results)
+
 
 demo_the_goodies()
--- test_grid.py
+++ test_grid.py
@@ -1,14 +1,18 @@
-from generate_result import * 
+from generate_result import *
+
 
 def run_ut(function_under_test, combinations, results):
     for combination, result in zip(combinations, results):
-        assert result == function_under_test(*combination) 
-        #ford 
-        #print("TRUE")
+        assert result == function_under_test(*combination)
+        # ford
+        # print("TRUE")
+
 
 def test_general_uts():
-    param_levels = [['A', 'B', 'C'], [1, 2], ['X', 'Y']]
-    combinations, results = generate_combination_and_results(param_levels, example_function)
+    param_levels = [["A", "B", "C"], [1, 2], ["X", "Y"]]
+    combinations, results = generate_combination_and_results(
+        param_levels, example_function
+    )
     print(combinations)
     print(results)
     run_ut(example_function, combinations, results)
